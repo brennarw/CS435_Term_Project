429dbb6df434b4f69cfb4c59d8225f35cbfe02ef
EthanSmithCS <ethansmithcs02@gmail.com>
2024-11-04 12:49:09 -0700

    first version of data organization

--- DataProcessing/src/main/java/org/processing/FlightDataProcessing.java
+++ DataProcessing/src/main/java/org/processing/FlightDataProcessing.java
@@ -20,6 +20,8 @@ package org.processing;
 import scala.Tuple2;
 import java.util.List;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Calendar;
 
 import org.apache.spark.api.java.JavaPairRDD;
 import org.apache.spark.api.java.JavaRDD;
@@ -34,16 +36,33 @@ import java.text.ParseException;
 
 public final class FlightDataProcessing {
 
-    public static void processFlightData(SparkSession spark, String flightDataInputPath){
+    public static void processFlightData(SparkSession spark, String flightDataInputPath, String airportDataInputPath){
         //Generate new flight data
         //FlightDate, Airline, origin, destination, cancelled, diverted, CRSDepTime (expected dep time), DepTime (actual: hhmm), DepDelayMinutes, DepDelay ....
         JavaRDD<String> flights = spark.read().textFile(flightDataInputPath).javaRDD();
+        JavaRDD<String> airports = spark.read().textFile(airportDataInputPath).javaRDD();
+        //String flightHeader = flights.first();
+        //String airportHeader = airports.first();
+        JavaPairRDD<String, String> flightsPair = flights
+                                                    .flatMapToPair(tuple -> {
+                                                    String[] cols = tuple.split(",", -1);
+                                                    return Arrays.asList(
+                                                        new Tuple2<>(cols[3], tuple), 
+                                                        new Tuple2<>(cols[4], tuple)  
+                                                    ).iterator();
+        });
+        System.out.println("beans" + flightsPair.collect());
+        JavaPairRDD<String, String> airportsPair = airports
+                                                .mapToPair(tuple -> new Tuple2<>(tuple.split(",", -1)[14], tuple));
+        JavaPairRDD<String,Tuple2<String,Optional<String>>> flightsJoined = flightsPair.leftOuterJoin(airportsPair);
 
+        System.out.println("Beans " + flightsJoined.collect());
         //fall is august (start) - november (end) -> month 8 - 11
         //filter out days that aren't considered fall 
-        JavaRDD<String> fallFlights = flights.filter(flight -> {
+        System.out.println(flightsPair.collect());
+        JavaPairRDD<String, String> fallFlights = flightsPair.filter(flight -> {
             try{
-                String[] attributes = flight.split(",");
+                String[] attributes = flight._1().split(",");
     
                 SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd");
     
@@ -64,7 +83,7 @@ public final class FlightDataProcessing {
     
     public static void main(String[] args) throws Exception {
 
-        if (args.length < 1) {
+        if (args.length < 2) {
             System.err.println("Usage: Need to input a hadoop file path.");
             System.exit(1);
         }
@@ -74,7 +93,7 @@ public final class FlightDataProcessing {
                 .appName("FlightDataProcessing").master("yarn")
                 .getOrCreate();
 
-        processFlightData(spark, "/435_TP/flight_data");
+        processFlightData(spark, args[0], args[1]);
         spark.stop();
     }
 }
