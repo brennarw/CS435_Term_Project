093eef2a6e22b0cdf6ebac80652e673653017b3f
EthanSmithCS <ethansmithcs02@gmail.com>
2024-11-07 10:54:08 -0700

    fixed arrival airports to sort on arrival time

--- DataProcessing/src/main/java/org/processing/FlightDataProcessing.java
+++ DataProcessing/src/main/java/org/processing/FlightDataProcessing.java
@@ -53,20 +53,7 @@ public final class FlightDataProcessing {
                                                     String year = date._2.split(",", -1)[0].substring(0, 4); //get year
                                                     return flightDate.after(sdf.parse(year + "-08-01")) && flightDate.before(sdf.parse(year +"-11-30")); //filter for fall (Aug 1 - Nov 30) only for years in the flight data (2018-2020)
                                                 })
-                                                .join(airportsPair);
-        //FORMAT: JavaPairRDD<<Airport Code>, Tuple2<Flight Data, Airport Data>>
-        JavaPairRDD<String,Tuple2<String,String>>  destPair = flights
-                                                .filter(row -> !row.equals(flightHeader)) //filter out column names
-                                                .mapToPair(tuple -> new Tuple2<>(tuple.split(",", -1)[3], tuple))  //map airport codes (ex. DEN) for destination airport from ind 3 as the key
-                                                .filter(date -> {
-                                                    Date flightDate = sdf.parse(date._2.split(",", -1)[0]); //find date of flight
-                                                    String year = date._2.split(",", -1)[0].substring(0, 4); //get year
-                                                    return flightDate.after(sdf.parse(year + "-08-01")) && flightDate.before(sdf.parse(year +"-11-30")); //filter for fall (Aug 1 - Nov 30) only for years in the flight data (2018-2020)
-                                                })
-                                                .join(airportsPair);
-        //FORMAT: JavaPairRDD<<Airport Code>, Tuple2<Flight Data, Airport Data>>
-        JavaPairRDD<String, Tuple2<String, String>> sunsetRDD = 
-                                                destPair.union(originPair) //combine origin and destination RDDs without any reduce by key
+                                                .join(airportsPair)
                                                 .filter(tuple -> {
                                                     try{
                                                         Integer.valueOf(tuple._2._1.split(",", -1)[7].substring(0 , tuple._2._1.split(",", -1)[7].length()-2)); //attempt to grab the hour:minute of departure at ind 7, filter out if non-numeric
@@ -105,7 +92,57 @@ public final class FlightDataProcessing {
                                                     }
                                                     return false;  //flight departure is not in range, discard record
                                                 });
-                                
+        //FORMAT: JavaPairRDD<<Airport Code>, Tuple2<Flight Data, Airport Data>>
+        JavaPairRDD<String,Tuple2<String,String>>  destPair = flights
+                                                .filter(row -> !row.equals(flightHeader)) //filter out column names
+                                                .mapToPair(tuple -> new Tuple2<>(tuple.split(",", -1)[3], tuple))  //map airport codes (ex. DEN) for destination airport from ind 3 as the key
+                                                .filter(date -> {
+                                                    Date flightDate = sdf.parse(date._2.split(",", -1)[0]); //find date of flight
+                                                    String year = date._2.split(",", -1)[0].substring(0, 4); //get year
+                                                    return flightDate.after(sdf.parse(year + "-08-01")) && flightDate.before(sdf.parse(year +"-11-30")); //filter for fall (Aug 1 - Nov 30) only for years in the flight data (2018-2020)
+                                                })
+                                                .join(airportsPair)
+                                                .filter(tuple -> {
+                                                    try{
+                                                        Integer.valueOf(tuple._2._1.split(",", -1)[10].substring(0 , tuple._2._1.split(",", -1)[10].length()-2)); //attempt to grab the hour:minute of departure at ind 7, filter out if non-numeric
+                                                        return true;
+                                                    }
+                                                    catch(Exception e){
+                                                        return false;
+                                                    }
+                                                } )
+                                                .filter(tuple -> {
+                                                    String[] splits = tuple._2._2.split(",", -1);
+                                                    String lat = splits[4]; //from airports dataset
+                                                    String lon = splits[5]; //from airports dataset
+                                                    Location location = new Location(lat, lon); //set location
+                                                    //generate sunset calculator
+                                                    SunriseSunsetCalculator calculator = new SunriseSunsetCalculator(location,  TimezoneMapper.latLngToTimezoneString(Double.parseDouble(lat), Double.parseDouble(lon)));
+                                                    Date date = sdf.parse(tuple._2._1.split(",", -1)[0]);
+                                                    Calendar flightDate = Calendar.getInstance();
+                                                    flightDate.setTime(date);
+                                                    String sunset = calculator.getOfficialSunsetForDate(flightDate); //get sunset
+                                                    SimpleDateFormat militartyFormat = new SimpleDateFormat("HH:mm");
+                                                    String dateString = tuple._2._1.split(",", -1)[10].substring(0 , tuple._2._1.split(",", -1)[10].length()-2); //get hour:minute of departure from flight date
+                                                    int hour = Integer.valueOf(dateString) / 100; //calculate hour
+                                                    int minute = Integer.valueOf(dateString) % 100; //calculate minute
+                                                    Date flightTime = militartyFormat.parse(hour + ":" + minute); //format to time object
+                                                    Date sunsetTime = militartyFormat.parse(sunset); //fortmat sunset to time object
+                                                    //Add range for grabbing flights (currently 30 min before and after sunset)
+                                                    Calendar calendar = Calendar.getInstance();
+                                                    calendar.setTime(sunsetTime);
+                                                    calendar.add(Calendar.MINUTE, 30);
+                                                    Date sunsetLate = calendar.getTime();
+                                                    calendar.add(Calendar.MINUTE, -60);
+                                                    Date sunsetEarly = calendar.getTime();
+                                                    if(flightTime.after(sunsetEarly) && flightTime.before(sunsetLate)){
+                                                        return true; //if flight arrival is within range, keep record
+                                                    }
+                                                    return false;  //flight arrival is not in range, discard record
+                                                });
+        //FORMAT: JavaPairRDD<<Airport Code>, Tuple2<Flight Data, Airport Data>>
+        JavaPairRDD<String, Tuple2<String, String>> sunsetRDD = destPair.union(originPair); //combine origin and destination RDDs without any reduce by key                 
+        //Write to file                        
         sunsetRDD.map(tuple -> tuple._1 + ": " + tuple._2).coalesce(1).saveAsTextFile("/435_TP/sunsetFlights");
     }
 
